944ebf53568b5a8400cd01b0172c6cde9039706c
Ankit Pokhrel <ankitpokhrel@gmail.com>
2015-09-05 14:57:12 +0545

    Remove class from hidden field

--- lib/admin/pages/general_settings.php
+++ lib/admin/pages/general_settings.php
@@ -95,7 +95,7 @@
 			</tbody>
 		</table>
 		<p class="submit">
-			<input type="hidden" name="_wfg_general_settings_submitted" value="Y" class="button button-primary" />
+			<input type="hidden" name="_wfg_general_settings_submitted" value="Y" />
 			<input type="submit" value="<?php echo WFG_Common_Helper::translate('Save Changes') ?>" class="button button-primary" />
 		</p>
 	</form>
