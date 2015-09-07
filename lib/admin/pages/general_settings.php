bb8300531901e2ffe3d57b0be98fa87882046a19
Ankit Pokhrel <ankitpokhrel@gmail.com>
2015-09-07 22:19:12 +0545

    Fix method name as per WordPress coding standard

--- lib/admin/pages/general_settings.php
+++ lib/admin/pages/general_settings.php
@@ -2,32 +2,32 @@
 		<div class="header clearfix">
 			<div class="left">
 				<?php
-					echo '<img src="' . plugins_url('img/wfg-logo.png', dirname(__FILE__)) . '" class="wfg-logo" />';
+					echo '<img src="' . plugins_url( 'img/wfg-logo.png', dirname(__FILE__) ) . '" class="wfg-logo" />';
 				?>
 			</div>
 			<div class="left">
-				<h1><?php echo WFG_Common_Helper::translate('WooCommerce Multiple Free Gift') ?></h1>
+				<h1><?php echo WFG_Common_Helper::translate( 'WooCommerce Multiple Free Gift' ) ?></h1>
 			</div>
 		<div class="right"></div>
 	</div>
-	<div class="options_group margin-top-20">	
+	<div class="options_group margin-top-20">
 		<p class="switcher">
-			<?php echo WFG_Common_Helper::translate('General Settings') ?>
+			<?php echo WFG_Common_Helper::translate( 'General Settings' ) ?>
 		</p>
 	</div>
 	<form class="wfg-general-settings" method="post" action="">
-		<?php wp_nonce_field('wfg_general_settings', '_wfg_general_nonce'); ?>
+		<?php wp_nonce_field( 'wfg_general_settings', '_wfg_general_nonce' ); ?>
 		<table class="form-table">
 			<tbody>
 				<tr>
 					<th scope="row">
-						<label for="popup_overlay"><?php echo WFG_Common_Helper::translate('Popup Overlay') ?></label>
+						<label for="popup_overlay"><?php echo WFG_Common_Helper::translate( 'Popup Overlay' ) ?></label>
 					</th>
 					<td>
 						<?php
 							$checked = '';
-							$overlay = WFG_Settings_Helper::get('popup_overlay', true, 'global_options');
-							if ($overlay) {
+							$overlay = WFG_Settings_Helper::get( 'popup_overlay', true, 'global_options' );
+							if( $overlay ) {
 								$checked = 'checked';
 							}
 						?>
@@ -40,13 +40,13 @@
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="popup_heading"><?php echo WFG_Common_Helper::translate('Popup Heading Text') ?></label>
+						<label for="popup_heading"><?php echo WFG_Common_Helper::translate( 'Popup Heading Text' ) ?></label>
 					</th>
 					<td>
 						<?php
-							$heading = WFG_Settings_Helper::get('popup_heading', false, 'global_options');
-							if ($heading === false) {
-								$heading = WFG_Common_Helper::translate('Choose your free gift');
+							$heading = WFG_Settings_Helper::get( 'popup_heading', false, 'global_options' );
+							if( $heading === false ) {
+								$heading = WFG_Common_Helper::translate( 'Choose your free gift' );
 							}
 						?>
 						<input type="text" name="_wfg_popup_heading" id="popup_heading" class="regular-text" value="<?php echo $heading ?>" />
@@ -54,13 +54,13 @@
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="popup_add_gift_text"><?php echo WFG_Common_Helper::translate('Add Gift Text') ?></label>
+						<label for="popup_add_gift_text"><?php echo WFG_Common_Helper::translate( 'Add Gift Text' ) ?></label>
 					</th>
 					<td>
 						<?php
-							$add_gift_text = WFG_Settings_Helper::get('popup_add_gift_text', false, 'global_options');
-							if ($add_gift_text === false) {
-								$add_gift_text = WFG_Common_Helper::translate('Add Gifts');
+							$add_gift_text = WFG_Settings_Helper::get( 'popup_add_gift_text', false, 'global_options' );
+							if( $add_gift_text === false ) {
+								$add_gift_text = WFG_Common_Helper::translate( 'Add Gifts' );
 							}
 						?>
 						<input type="text" name="_wfg_popup_add_gift_text" id="popup_add_gift_text" class="regular-text" value="<?php echo $add_gift_text ?>" />
@@ -68,13 +68,13 @@
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="popup_cancel_text"><?php echo WFG_Common_Helper::translate('Cancel Text') ?></label>
+						<label for="popup_cancel_text"><?php echo WFG_Common_Helper::translate( 'Cancel Text' ) ?></label>
 					</th>
 					<td>
 						<?php
-							$cancel_text = WFG_Settings_Helper::get('popup_cancel_text', false, 'global_options');
-							if ($cancel_text === false) {
-								$cancel_text = WFG_Common_Helper::translate('No Thanks');
+							$cancel_text = WFG_Settings_Helper::get( 'popup_cancel_text', false, 'global_options' );
+							if( $cancel_text === false ) {
+								$cancel_text = WFG_Common_Helper::translate( 'No Thanks' );
 							}
 						?>
 						<input type="text" name="_wfg_popup_cancel_text" id="popup_cancel_text" class="regular-text" value="<?php echo $cancel_text ?>" />
@@ -82,13 +82,13 @@
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="invalid_condition"><?php echo WFG_Common_Helper::translate('Invalid Gift Condition Text') ?></label>
+						<label for="invalid_condition"><?php echo WFG_Common_Helper::translate( 'Invalid Gift Condition Text' ) ?></label>
 					</th>
 					<td>
 						<?php
-							$invalidText = WFG_Settings_Helper::get('invalid_condition_text', false, 'global_options');
-							if ($invalidText === false) {
-								$invalidText = WFG_Common_Helper::translate('Gift items removed as gift criteria isn\'t fulfilled');
+							$invalidText = WFG_Settings_Helper::get( 'invalid_condition_text', false, 'global_options' );
+							if( $invalidText === false ) {
+								$invalidText = WFG_Common_Helper::translate( 'Gift items removed as gift criteria isn\'t fulfilled' );
 							}
 						?>
 						<input type="text" name="_wfg_invalid_condition_text" id="invalid_condition" class="regular-text"
@@ -99,7 +99,7 @@
 		</table>
 		<p class="submit">
 			<input type="hidden" name="_wfg_general_settings_submitted" value="Y" />
-			<input type="submit" value="<?php echo WFG_Common_Helper::translate('Save Changes') ?>" class="button-primary" />
+			<input type="submit" value="<?php echo WFG_Common_Helper::translate( 'Save Changes' ) ?>" class="button-primary" />
 		</p>
 	</form>
 </div>
