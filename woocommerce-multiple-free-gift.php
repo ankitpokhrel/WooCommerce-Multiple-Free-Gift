352742dfea19a01295dc88dca532c12ee39e34d0
Ankit Pokhrel <ankitpokhrel@gmail.com>
2015-09-07 23:57:37 +0545

    Fix coding style in woocommerce-multiple-free-gift.php

--- woocommerce-multiple-free-gift.php
+++ woocommerce-multiple-free-gift.php
@@ -13,27 +13,27 @@
 */
 
 //Avoid direct calls to this file
-if ( !defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	die( 'Access Forbidden' );
 }
 
-define( 'PLUGIN_BASE', plugin_basename(__FILE__) );
+define( 'PLUGIN_BASE', plugin_basename( __FILE__ ) );
 define( 'PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'PRO_URL', 'http://ankitpokhrel.com.np/blog/downloads/woocommerce-multiple-free-gift-plugin-pro/');
+define( 'PRO_URL', 'http://ankitpokhrel.com.np/blog/downloads/woocommerce-multiple-free-gift-plugin-pro/' );
 
-include "lib/helpers/WFG_Common_Helper.class.php";
-include "lib/helpers/WFG_Settings_Helper.class.php";
-include "lib/helpers/WFG_Product_Helper.class.php";
-include "lib/helpers/WFG_Criteria_Helper.class.php";
-include "lib/admin/WFG_Admin.class.php";
-include "lib/admin/WFG_Single_Gift.class.php";
-include "lib/WFG_Frontend.class.php";
-include "lib/Woocommerce_Multiple_Free_Gift.class.php";
+include 'lib/helpers/WFG_Common_Helper.class.php';
+include 'lib/helpers/WFG_Settings_Helper.class.php';
+include 'lib/helpers/WFG_Product_Helper.class.php';
+include 'lib/helpers/WFG_Criteria_Helper.class.php';
+include 'lib/admin/WFG_Admin.class.php';
+include 'lib/admin/WFG_Single_Gift.class.php';
+include 'lib/WFG_Frontend.class.php';
+include 'lib/Woocommerce_Multiple_Free_Gift.class.php';
 
 //plugin activation hook
-register_activation_hook(  __FILE__ , array('Woocommerce_Multiple_Free_Gift', 'wfg_activate') );
+register_activation_hook( __FILE__ , array( 'Woocommerce_Multiple_Free_Gift', 'wfg_activate' ) );
 
 /** Initialize the awesome */
 new Woocommerce_Multiple_Free_Gift();
